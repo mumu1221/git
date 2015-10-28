8eaa3dd8ddb5cf838112f3a703fa43e559861313
mumu <2855971058@qq.com>
2015-10-28 16:24:54 +0800

    int -> elementtype

--- list.c
+++ list.c
@@ -1,4 +1,4 @@
-/*************************************************************************
+/*************************************************
 	> File Name: 1.c
 	> Author: 
 	> Mail: 
@@ -8,20 +8,21 @@
 #include<stdio.h>
 typedef struct node *ptr;
 typedef ptr list;
-ptr find(int x,list l);
-ptr creat(int m);//creat a linklist include m element;
-void insert(int x,list l,ptr p);//insert an element after p in the list l;
-void delete(int x,list l);//delete the element x in the list l;
-int isempty(list l);//if the list is empty,return 1,else return 0; 
-int islast(ptr p);//if the ptr is last return 1,else return 0;
-ptr findpre(int x,list l);//return the element before the x element; 
+typedef elementtype int;
+ptr find(elementtype x,list l);
+ptr creat(elementtype m);//creat a linklist include m element;
+void insert(elementtype x,list l,ptr p);//insert an element after p in the list l;
+void delete(elementtype x,list l);//delete the element x in the list l;
+elementtype isempty(list l);//if the list is empty,return 1,else return 0; 
+elementtype islast(ptr p);//if the ptr is last return 1,else return 0;
+ptr findpre(elementtype x,list l);//return the element before the x element; 
 void deletelist(list l);
 
 struct node {
-    int ele;
+    elementtype ele;
     ptr next;
 };
-int main(){
+elementtype main(){
     ptr l=creat(5);
     ptr a=findpre(20,l);
     insert(50,l,a);
@@ -29,7 +30,7 @@ int main(){
     long b=(long)a->next->ele;
     printf("%ld",b);
 }
-ptr find(int x,list l){
+ptr find(elementtype x,list l){
     while(l->ele!=x && l!=NULL){
         l=l->next;
     }
@@ -38,32 +39,32 @@ ptr find(int x,list l){
 
 
 
-void insert(int x,list l,ptr p){
+void insert(elementtype x,list l,ptr p){
     ptr tmp = (ptr)malloc(sizeof(struct node));
     tmp->ele=x;
     tmp->next=p->next;
     p->next=tmp;
 }
 
-ptr findpre(int x,list l){
+ptr findpre(elementtype x,list l){
     while(l->next!=NULL && l->next->ele!=x){
         l=l->next;
     }
     return l;
 }
 
-void delete(int x,list l){
+void delete(elementtype x,list l){
     ptr p =findpre(x,l);
     ptr tmp =p->next;
     p->next=tmp->next;
     free(tmp);
 }
 
-ptr creat(int m){
+ptr creat(elementtype m){
     ptr l=(ptr)malloc(sizeof(struct node));
     ptr p=(ptr)malloc(sizeof(struct node));
     l->next=p;
-    int i =0;
+    elementtype i =0;
     while(i<m){
         scanf("%d",&p->ele);
         p->next=(ptr)malloc(sizeof(struct node));
